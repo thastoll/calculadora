e2b398890a17462918403048e3017b0c799fa7cb
thastoll <thabatastoll@gmail.com>
2026-01-14 18:52:01 -0300

    criando calculadora

---
+++ app/calculadora/page.tsx
@@ -0,0 +1,9 @@
+import { Calculator } from "../components/Calculator";
+
+export default function Home() {
+  return (
+    <main style={{ padding: 24 }}>
+      <Calculator />
+    </main>
+  );
+}
---
+++ app/components/Calculator.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export function Calculator() {
+  return (
+    <div>
+      <h2>Minha Calculadora</h2>
+      <p>Calculadora funcionando 🎉</p>
+    </div>
+  );
+}
